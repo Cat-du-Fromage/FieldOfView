e39de2ac3fe207d95198e28cacf047b5cde786d4
Florian <florian.duruz@gmail.com>
2024-06-18 21:03:12 +0200

    base Setup

---
+++ Assets/Code/FieldOfViewComponent.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace FieldOfView
+{
+    public class FieldOfViewComponent : MonoBehaviour
+    {
+        [SerializeField] private float Range;
+        [SerializeField] private float SideAngles;
+        [SerializeField] private float WidthLength;
+        
+        
+    }
+}
