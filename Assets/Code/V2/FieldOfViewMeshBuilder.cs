74b67a0284d39471f073b0f310176b043ecd60bd
Florian <florian.duruz@gmail.com>
2024-07-04 13:59:37 +0200

    Update FieldOfViewMeshBuilder.cs

--- Assets/Code/V2/FieldOfViewMeshBuilder.cs
+++ Assets/Code/V2/FieldOfViewMeshBuilder.cs
@@ -1,10 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Collections;
 using Unity.Mathematics;
 using UnityEngine;
 
 namespace FieldOfView
 {
+    public struct MeshInfos
+    {
+        
+    }
+    
     [RequireComponent(typeof(FieldOfViewComponent), typeof(MeshFilter))]
     public class FieldOfViewMeshBuilder : MonoBehaviour
     {
@@ -29,5 +35,31 @@ namespace FieldOfView
         {
             Mesh fovMesh = meshFilter.sharedMesh;
         }
+        /*
+        public void SetBorderVertices(NativeArray<float3> vertices, int borderVerticesCount)
+        {
+            int borderOuterStep = 0;
+            int borderInnerStep = 0;
+            
+            int halfVertices = borderVerticesCount / 2;
+            for (int i = 0; i < halfVertices; i++)
+            {
+                float2 baseLeftOffset = i * BorderDirection;
+                //Left Outer
+                float3 leftOuter = (OuterBorderStart + baseLeftOffset * borderOuterStep).x0y();
+                vertices[i * 2] = leftOuter;
+                //Left Inner
+                float3 leftInner = (InnerBorderStart + baseLeftOffset * borderInnerStep).x0y();
+                vertices[i * 2 + 1] = leftInner;
+                
+                //Right Outer
+                int innerRightIndex = vertices.Length - (1 + i * 2);
+                vertices[innerRightIndex] = leftInner - 2 * math.project(leftInner, math.right());
+                //Right Inner
+                int outerRightIndex = innerRightIndex - 1; // vertices.Length - (1 + i * 2) - 1;
+                vertices[outerRightIndex] = leftOuter - 2 * math.project(leftOuter, math.right());
+            }
+        }
+        */
     }
 }
